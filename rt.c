a9a40ca4eb76756e022921d791e42adb24ccd0a7
Victor "multun" Collod <victor.collod@epita.fr>
2020-12-08 00:40:06 +0100

    refactor the rendering loop

--- rt.c
+++ rt.c
@@ -6,13 +6,12 @@
 #include <string.h>
 
 #include "bmp.h"
-#include "image.h"
-#include "vec3.h"
 #include "camera.h"
+#include "image.h"
+#include "phong_material.h"
 #include "scene.h"
 #include "sphere.h"
-#include "phong_material.h"
-
+#include "vec3.h"
 
 struct rgb_pixel normal_color(const struct vec3 *normal)
 {
@@ -32,7 +31,6 @@ struct rgb_pixel normal_color(const struct vec3 *normal)
     return res;
 }
 
-
 /*
 ** The color of a light is encoded inside a float, from 0 to +inf,
 ** where 0 is no light, and +inf a lot more light. Unfortunately,
@@ -50,7 +48,6 @@ static inline uint8_t translate_light_component(double light_comp)
     return light_comp * 255;
 }
 
-
 /*
 ** Converts an rgb floating point light color to 24 bit rgb.
 */
@@ -62,7 +59,6 @@ struct rgb_pixel rgb_color_from_light(const struct vec3 *light)
     res.b = translate_light_component(light->z);
     return res;
 }
-
 
 static void build_test_scene(struct scene *scene, double aspect_ratio)
 {
@@ -103,6 +99,51 @@ static void build_test_scene(struct scene *scene, double aspect_ratio)
     material_put(&red_material->base);
 }
 
+/* For all the pixels of the image, try to find the closest object
+** intersecting the camera ray. If an object is found, shade the pixel to
+** find its color.
+*/
+static void render_pixel(struct rgb_image *image, struct scene *scene, size_t x,
+                         size_t y)
+{
+    // find the position of the current pixel in the image plane
+    // camera_cast_ray takes camera relative positions, from -0.5 to 0.5 for
+    // both axis
+    double cam_x = ((double)x / image->width) - 0.5;
+    double cam_y = ((double)y / image->height) - 0.5;
+
+    // find the starting point and direction of this ray
+    struct ray ray;
+    camera_cast_ray(&ray, &scene->camera, cam_x, cam_y);
+
+    // we will now try to find the closest object in the scene
+    // intersecting this ray
+    struct object_intersection closest_intersection;
+    double closest_intersection_dist = INFINITY;
+
+    for (size_t i = 0; i < object_vect_size(&scene->objects); i++)
+    {
+        struct object *obj = object_vect_get(&scene->objects, i);
+        struct object_intersection intersection;
+        // if there's no intersection between the ray and this object, skip it
+        double intersection_dist = obj->intersect(&intersection, obj, &ray);
+        if (intersection_dist >= closest_intersection_dist)
+            continue;
+
+        closest_intersection_dist = intersection_dist;
+        closest_intersection = intersection;
+    }
+
+    // if the intersection distance is infinite, do not shade the pixel
+    if (isinf(closest_intersection_dist))
+        return;
+
+    struct material *mat = closest_intersection.material;
+    struct vec3 pix_color
+        = mat->shade(mat, &closest_intersection.location, scene, &ray);
+    rgb_image_set(image, x, y, rgb_color_from_light(&pix_color));
+}
+
 int main(int argc, char *argv[])
 {
     int rc;
@@ -110,7 +151,8 @@ int main(int argc, char *argv[])
     if (argc != 2)
         errx(1, "Usage: OUTPUT.bmp");
 
-    // initialize the frame buffer (the buffer that will store the result of the rendering)
+    // initialize the frame buffer (the buffer that will store the result of the
+    // rendering)
     struct rgb_image *image = rgb_image_alloc(1920, 1080);
 
     // set all the pixels of the image to black
@@ -124,57 +166,21 @@ int main(int argc, char *argv[])
     scene_init(&scene);
     build_test_scene(&scene, aspect_ratio);
 
-    // for all the pixels of the image, try to find the closest object intersecting
-    // the camera ray. If an object is found, shade the pixel to find its color.
+    // render all pixels
     for (size_t y = 0; y < image->height; y++)
         for (size_t x = 0; x < image->width; x++)
-        {
-            // find the position of the current pixel in the image plane
-            // camera_cast_ray takes camera relative positions, from -0.5 to 0.5 for both axis
-            double cam_x = ((double)x / image->width) - 0.5;
-            double cam_y = ((double)y / image->height) - 0.5;
+            render_pixel(image, &scene, x, y);
 
-            // find the starting point and direction of this ray
-            struct ray ray;
-            camera_cast_ray(&ray, &scene.camera, cam_x, cam_y);
-
-            // we will now try to find the closest object in the scene intersecting this ray
-            struct object_intersection closest_intersection;
-            double closest_intersection_dist = INFINITY;
-
-            for (size_t i = 0; i < object_vect_size(&scene.objects); i++)
-            {
-                struct object *obj = object_vect_get(&scene.objects, i);
-                struct object_intersection intersection;
-                // if there's no intersection between the ray and this object, skip it
-                double intersection_dist = obj->intersect(&intersection, obj, &ray);
-                if (intersection_dist >= closest_intersection_dist)
-                    continue;
-
-                closest_intersection_dist = intersection_dist;
-                closest_intersection = intersection;
-            }
-
-            // if the intersection distance is infinite, do not shade the pixel
-            if (isinf(closest_intersection_dist))
-                continue;
-
-            struct material *mat = closest_intersection.material;
-            struct vec3 pix_color = mat->shade(mat, &closest_intersection.location, &scene, &ray);
-            rgb_image_set(image, x, y, rgb_color_from_light(&pix_color));
-        }
-
+    // write the rendered image to a bmp file
     FILE *fp = fopen(argv[1], "w");
     if (fp == NULL)
         err(1, "failed to open the output file");
 
     rc = bmp_write(image, ppm_from_ppi(80), fp);
-
     fclose(fp);
 
+    // release resources
     scene_destroy(&scene);
-
     free(image);
-
     return rc;
 }
